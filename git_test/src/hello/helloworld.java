0375c79b38054d07fb1c91efeef67ace1df1d25e
tuxusheng <2239557880@qq.com>
2019-06-30 19:32:46 +0800

    添加的新项目到github

---
+++ git_test/src/hello/helloworld.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("你好啊");
+        System.out.println("你好啊");
+        System.out.println("你好啊");
+        System.out.println("你好啊");
+        System.out.println("你好啊");
+
+    }
+}
